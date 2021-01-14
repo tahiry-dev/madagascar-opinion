965e472762c6dc5dfa13c29d330c8e5d91287332
tahiry-dev <tahirycontact@gmail.com>
2021-01-14 14:10:28 +0300

    implement creation of account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,22 @@
 class UsersController < ApplicationController
+    
     def  new
-        
+      @user = User.new
     end
+
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        flash[:success] = "Welcome to Madagascar-Opinion"
+         #handle success
+      else
+        render :new
+      end
+    end
+
+    private
+        def user_params
+            params.require(:user).permit(:username, :fullname)
+        end
     
 end
