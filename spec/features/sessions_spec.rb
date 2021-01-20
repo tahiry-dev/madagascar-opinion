762b6535669ccce5652860f50546dbd7ce2a7a74
tahiry-dev <tahirycontact@gmail.com>
2021-01-20 14:26:56 +0300

    fix linters error

--- spec/features/sessions_spec.rb
+++ spec/features/sessions_spec.rb
@@ -1,30 +1,28 @@
 require 'rails_helper'
 
 RSpec.feature 'Sessions', type: :feature do
-    before(:each) do
-        @user = User.new(username: 'Tahiry', fullname:'RANDRIAMIARINTSOA')
-        @user.save
-    end
+  before(:each) do
+    @user = User.new(username: 'Tahiry', fullname: 'RANDRIAMIARINTSOA')
+    @user.save
+  end
 
-    scenario 'Registered user could login with username' do
-      visit login_path
-    
-      fill_in 'Username', with: 'Tahiry'
-    
-      click_button 'Log into my account'
-        expect(page).to have_content('Welcome Back!')
-    end
+  scenario 'Registered user could login with username' do
+    visit login_path
 
-    scenario 'Registered user could logout' do
+    fill_in 'Username', with: 'Tahiry'
 
-      visit login_path
-    
-      fill_in 'Username', with: 'Tahiry'
+    click_button 'Log into my account'
+    expect(page).to have_content('Welcome Back!')
+  end
 
-      click_button 'Log into my account'
+  scenario 'Registered user could logout' do
+    visit login_path
 
-      click_link 'LOGOUT'
-         expect(page).to have_content('Thanks for your opinions, see you later!')
-    end
+    fill_in 'Username', with: 'Tahiry'
 
+    click_button 'Log into my account'
+
+    click_link 'LOGOUT'
+    expect(page).to have_content('Thanks for your opinions, see you later!')
+  end
 end
