6be6f6d2201bee470ec9976ed3f0749df9a51422
tahiry-dev <tahirycontact@gmail.com>
2021-01-20 13:52:38 +0300

    refactor the view to avoid logic

---
+++ app/helpers/menus_helper.rb
@@ -0,0 +1,27 @@
+module MenusHelper
+
+    def should_follow?(user)
+
+        if user != @current_user && !@current_user.followings.include?(user)
+            render "shared/to_follow", user:user
+        end
+    end
+
+    def check_for_photo(user)
+        if user.photo.attached?
+            image_tag user.photo, class: "who-to-follow-img"
+        else
+            image_tag("navigation/home.jpeg", class: "who-to-follow-img") 
+        end
+    end
+
+    def check_for_cover_image(user)
+        if user.coverimage.attached?
+            image_tag @user.coverimage, class: "cover-img" 
+        else
+            image_tag("navigation/cover.jpg", class: "cover-img")
+        end
+    end
+
+    
+end
